e2263740d8291e9d5a3f8de7c847056c93b47de0
Omid Nikrah <omidnikrah@gmail.com>
2020-07-29 13:38:58 +0430

    add height to wrapper svg

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ http.createServer(async (req, res) => {
     articles = [responseJson.items[0]];
   }
 
-  let result = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="390px" version="1.2">';
+  let result = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="390px" version="1.2" height="${articles.length * 120}">`;
 
   await asyncForEach(articles, async (article, index) => {
     const articleCard = await ArticleCard(article);
